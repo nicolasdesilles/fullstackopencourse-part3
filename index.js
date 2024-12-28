0763eda8b4420326aa84ac40f8bb369632deed47
Nicolas Désilles <nicolas@ndlabs.fr>
2024-12-28 20:45:16 +0100

    Finished Exercise 3.9

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 morgan.token('contents', (request, response) => {
@@ -13,6 +14,7 @@ morgan.token('contents', (request, response) => {
   }
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :contents'))
 
